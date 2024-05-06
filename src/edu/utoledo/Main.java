1002b24fe7992b8fddec3f25d0d6cf5157992639
carrdylan52 <carrdylan52@users.noreply.github.com>
2024-05-06 15:04:55 -0400

    Closes Issue #1
    Changed `addTwoPlusTwo()` method to `addTwoInts(int a, int b)`

--- src/edu/utoledo/Main.java
+++ src/edu/utoledo/Main.java
@@ -3,11 +3,11 @@ package edu.utoledo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("2+3=" + addTwoPlusTwo());
+        System.out.println("Hello World! This is Dylan Carr!");
+        System.out.println("2+3=" + addTwoInts(2,3));
     }
 
-    public static int addTwoPlusTwo() {
-        return 2 + 3;
+    public static int addTwoInts(int a, int b) {
+        return a + b;
     }
 }
